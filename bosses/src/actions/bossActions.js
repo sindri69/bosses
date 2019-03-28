6adf16d554502c365fe34cc5ca57f996c14c715d
sindri69 <sindrii17@ru.is>
2019-03-28 16:51:54 +0000

    boss actions

--- bosses/src/actions/bossActions.js
+++ bosses/src/actions/bossActions.js
@@ -1,4 +1,4 @@
-import { CREATE_BOSS } from '../constants';
+import { CREATE_BOSS, GET_BOSSES, GET_BOSS, UPDATE_BOSS, DELETE_BOSS } from '../constants';
 
 export const createBoss = newBoss => {
   console.log(newBoss);
@@ -16,4 +16,27 @@ export const UPDATE_BOSS = boss => {
   };
 };
 
-export const 
+export const DELETE_BOSS = boss => {
+  console.log(boss);
+  return {
+    type: DELETE_BOSS,
+    payload: bossID
+  };
+};
+
+export const GET_BOSS = boss => {
+  console.log(boss);
+  return {
+    type: GET_BOSS,
+    payload: boss
+  };
+};
+
+export const GET_BOSSES = bosses => {
+  console.log(bosses);
+  return {
+    type: GET_BOSSES,
+    payload: bosses
+  };
+};
+
